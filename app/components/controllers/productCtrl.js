327d8e81be1ad2b68d15fe553ab3b1440b7a9d95
rohan <rohan@valetez.com>
2018-09-03 16:36:21 +0530

    adding the files

---
+++ app/components/controllers/productCtrl.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('myApp.product', ['ngRoute'])
+
+
+.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/product', {
+            templateUrl: 'components/pages/product.html',
+            controller: 'productCtrl'
+        });
+    }])
+    .controller('productCtrl', [function() {
+        console.log('product ctrl called');
+
+    }
+    ]);
